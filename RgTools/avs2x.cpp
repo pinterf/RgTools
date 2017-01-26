3210d8dbcfc08fa4a4ba946f5ddd644e20000b6b
Pinterf <Pinterf@gmail.com>
2017-01-26 20:54:01 +0100

    Clense param def: add [reduceflicker]b[planar]b[cache]i

--- RgTools/avs2x.cpp
+++ RgTools/avs2x.cpp
@@ -12,7 +12,7 @@ extern "C" __declspec(dllexport) const char* __stdcall AvisynthPluginInit3(IScri
 
     env->AddFunction("RemoveGrain", "c[mode]i[modeU]i[modeV]i[planar]b", Create_RemoveGrain, 0);
     env->AddFunction("Repair", "cc[mode]i[modeU]i[modeV]i[planar]b", Create_Repair, 0);
-    env->AddFunction("Clense", "c[previous]c[next]c[grey]b", Create_Clense, 0);
+    env->AddFunction("Clense", "c[previous]c[next]c[grey]b[reduceflicker]b[planar]b[cache]i", Create_Clense, 0);
     env->AddFunction("ForwardClense", "c[grey]b", Create_ForwardClense, 0);
     env->AddFunction("BackwardClense", "c[grey]b", Create_BackwardClense, 0);
     env->AddFunction("VerticalCleaner", "c[mode]i[modeU]i[modeV]i[planar]b", Create_VerticalCleaner, 0);

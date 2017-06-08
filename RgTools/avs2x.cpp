6ad87c46def67f62e88be9c8373dacec2d804c30
Pinterf <pinterfer@gmail.com>
2017-06-08 17:30:46 +0200

    Revert a prev. commit, planar defined twice

--- RgTools/avs2x.cpp
+++ RgTools/avs2x.cpp
@@ -13,8 +13,8 @@ extern "C" __declspec(dllexport) const char* __stdcall AvisynthPluginInit3(IScri
     env->AddFunction("RemoveGrain", "c[mode]i[modeU]i[modeV]i[planar]b", Create_RemoveGrain, 0);
     env->AddFunction("Repair", "cc[mode]i[modeU]i[modeV]i[planar]b", Create_Repair, 0);
     env->AddFunction("Clense", "c[previous]c[next]c[grey]b[reduceflicker]b[planar]b[cache]i", Create_Clense, 0);
-    env->AddFunction("ForwardClense", "c[grey]b[planar]b[cache]i[planar]b", Create_ForwardClense, 0);
-    env->AddFunction("BackwardClense", "c[grey]b[planar]b[cache]i[planar]b", Create_BackwardClense, 0);
+    env->AddFunction("ForwardClense", "c[grey]b[planar]b[cache]i", Create_ForwardClense, 0);
+    env->AddFunction("BackwardClense", "c[grey]b[planar]b[cache]i", Create_BackwardClense, 0);
     env->AddFunction("VerticalCleaner", "c[mode]i[modeU]i[modeV]i[planar]b", Create_VerticalCleaner, 0);
     return "Itai, onii-chan!";
 }

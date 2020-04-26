bd9a3504b05dcdc9b9428b8a119c878116b3c429
Pinterf <pinterfer@gmail.com>
2020-04-26 14:21:30 +0200

    RemoveGrain opt for general CPU branch test

--- RgTools/avs2x.cpp
+++ RgTools/avs2x.cpp
@@ -11,7 +11,7 @@ const AVS_Linkage *AVS_linkage = nullptr;
 extern "C" __declspec(dllexport) const char* __stdcall AvisynthPluginInit3(IScriptEnvironment* env, const AVS_Linkage* const vectors) {
     AVS_linkage = vectors;
 
-    env->AddFunction("RemoveGrain", "c[mode]i[modeU]i[modeV]i[planar]b[optavx2]b", Create_RemoveGrain, 0);
+    env->AddFunction("RemoveGrain", "c[mode]i[modeU]i[modeV]i[planar]b[opt]i", Create_RemoveGrain, 0);
     env->AddFunction("Repair", "cc[mode]i[modeU]i[modeV]i[planar]b", Create_Repair, 0);
     env->AddFunction("Clense", "c[previous]c[next]c[grey]b[reduceflicker]b[planar]b[cache]i", Create_Clense, 0);
     env->AddFunction("ForwardClense", "c[grey]b[planar]b[cache]i", Create_ForwardClense, 0);
